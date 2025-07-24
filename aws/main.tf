1e55b3850d1a6027904453fd061206d40df5a13c
BSK-CHN <awstrainingchn@gmail.com>
2025-07-24 17:53:58 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -2,25 +2,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_iam_user" "anbu" {
+  name = "anbu"
+  force_destroy = true
+}
+
 resource "aws_iam_user_login_profile" "anbu_login" {
-  user = aws_iam_user.anbu_user.name
+  user = aws_iam_user.anbu.name
+
   lifecycle {
     prevent_destroy = false
   }
 }
 
 resource "aws_iam_user_policy_attachment" "anbu_policy_attach" {
-  user       = aws_iam_user.anbu_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" # or any attached policy
+  user       = aws_iam_user.anbu.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+
   lifecycle {
     prevent_destroy = false
   }
 }
 
-resource "aws_iam_user" "anbu_user" {
-  name = "anbu"
-  force_destroy = true
+resource "aws_iam_access_key" "anbu_key" {
+  user = aws_iam_user.anbu.name
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
+
 
 
 
