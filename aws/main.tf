c6ddc1aae8a5f7b0fd24e6f1e80701996f242e3c
BSK-CHN <awstrainingchn@gmail.com>
2025-07-24 17:45:31 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,31 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "your-organization-name"  # Replace with your Terraform Cloud org
+    workspaces {
+      name = "your-workspace-name"           # Replace with your workspace name
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
+# Define a sensitive variable for password
+variable "anbu_password" {
+  type      = string
+  sensitive = true
+}
+
+# Create IAM user
 resource "aws_iam_user" "anbu_user" {
   name = "anbu"
 }
 
+# Assign login profile with password
 resource "aws_iam_user_login_profile" "anbu_login" {
-  user    = aws_iam_user.anbu_user.name
-  password = "Anbu@12345"  # Change to a strong password!
+  user                    = aws_iam_user.anbu_user.name
+  password                = var.anbu_password
   password_reset_required = true
-}
-
-resource "aws_iam_group" "admin_group" {
-  name = "AdminGroup"
-}
-
-resource "aws_iam_group_policy_attachment" "admin_group_attach" {
-  group      = aws_iam_group.admin_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-resource "aws_iam_user_group_membership" "anbu_group_membership" {
-  user = aws_iam_user.anbu_user.name
-  groups = [
-    aws_iam_group.admin_group.name
-  ]
 }
 
 
